e25c7ef29bb41d921efea6a4891715bbccc3c800
DylanRussell <dylanrus@gmail.com>
2017-09-03 11:58:01 -0400

    Removed a pyc file, added some base html pages, a css page, and added the add patient view

--- nursingHomeApp/views/wireframe.py
+++ nursingHomeApp/views/wireframe.py
@@ -5,3 +5,8 @@ from flask import render_template
 @app.route('/')
 def base():
     return render_template('base.html')
+
+
+@app.route('/patient')
+def patient():
+    return render_template('add_patient.html')
