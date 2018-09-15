5bbca18fae077d31519d11da9e00722e52ece27f
Dylan Russell <dylanrussell@Dylans-MacBook-Pro.local>
2018-09-15 15:17:54 -0400

    more changes related to reorg of app to use blueprints and app factory pattern

---
+++ nursingHomeApp/common_validators.py
@@ -0,0 +1,12 @@
+def STR_2_NONE(x):
+    return x or None
+
+
+def is_valid_phone(form, field):
+    if form.phone.data:
+        try:
+            int(form.phone.data)
+        except ValueError:
+            raise ValidationError('Please only include digits in this field')
+        if len(form.phone.data) not in {10, 11}:
+            raise ValidationError('Phone number must be 10 or 11 digits long')
