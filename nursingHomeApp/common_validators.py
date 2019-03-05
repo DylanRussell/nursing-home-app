cd615f28f5d9a34d8c1ba4d1f58cde38116678a9
Dylan Russell <dylanrus@gmail.com>
2019-03-05 16:07:28 -0500

    add docstrings to functions, use field.data instead of form.{}.data so is_valid_phone validator more reusable

--- nursingHomeApp/common_validators.py
+++ nursingHomeApp/common_validators.py
@@ -1,15 +1,22 @@
 from wtforms.validators import ValidationError
 
 
-def STR_2_NONE(x):
-    return x or None
+def STR_2_NONE(string):
+    """Converts empty string to None, which corresponds to null in mysql"""
+    return string or None
 
 
 def is_valid_phone(form, field):
-    if form.phone.data:
+    """Is the phone number of valid length and contains only digits"""
+    if field.data:
         try:
-            int(form.phone.data)
+            int(field.data)
         except ValueError:
             raise ValidationError('Please only include digits in this field')
-        if len(form.phone.data) not in {10, 11}:
+        if len(field.data) not in {10, 11}:
             raise ValidationError('Phone number must be 10 or 11 digits long')
+
+def is_gt_zero(form, field):
+    """Raises validation error if field <= 0"""
+    if field.data <= 0:
+        raise ValidationError('This field must contain a positive integer.')
